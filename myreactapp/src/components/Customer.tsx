cb61ffa2cdcbab53090a592285ff4999b903ed95
LourenceQ <lawrenceqf@gmail.com>
2023-06-07 21:31:15 -0300

    Implementando type e component exemplo

---
+++ myreactapp/src/components/Customer.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+type Person = {
+    firstName: string;
+    lastName: string;
+    age: number;
+    isLegal: boolean;
+};
+
+const Customer = (props: Person) => {
+    const {firstName, lastName, age, isLegal} = props;
+
+    return (
+        <div>
+            <h1>Hello React</h1>
+        </div>
+    );
+};
+
+export default Customer;
---
+++ myreactapp/src/types/Person.ts
@@ -0,0 +1,8 @@
+import React from "react";
+
+type Person = {
+    firstName: string;
+    lastName: string;
+    age: number;
+    isLegal: boolean;
+};
