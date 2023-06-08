896fa2557f43bc1c7ffce349727684dc306b247a
LourenceQ <lawrenceqf@gmail.com>
2023-06-08 19:43:50 -0300

    Implementando simples evento em botão

--- myreactapp/src/App.tsx
+++ myreactapp/src/App.tsx
@@ -1,18 +1,28 @@
-import React, { useState } from 'react';
-import './App.css';
-import Customer from './components/Customer';
+import React, { useState } from "react";
+import "./App.css";
+import Customer from "./components/Customer";
 
 function App() {
+  const handleAlert = () => {
+    alert("I'm a button!");
+  };
+
   const [title, setTitle] = useState("Practical React Enterprise");
   return (
     <div className="App">
-      <h1 style={{color: "blue", marginBottom: "5rem", }}>{title}</h1>
-      <Customer 
-        firstName = "Lourence"
-        lastName = "Doe"
-        age={21}
-        isLegal={true}
-      />
+      <button
+        style={{
+          color: "#ffff",
+          height: "5rem",
+          width: "10rem",
+          backgroundColor: "tomato",
+          borderRadius: "5px",
+          fontSize: "18px",
+        }}
+        onClick={handleAlert}
+      >
+        Click me
+      </button>
     </div>
   );
 }
